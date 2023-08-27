3139ebc4fc27e1d4aadc1a6dc098da1108a4de53
mariamsinger <mariamsinger@vt.edu>
2023-08-27 15:43:42 -0400

    added pagination and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import RestaurantCard from "./components/RestaurantCard";
 import restaurantData from "./assets/restaurant_data.json";
 import { RestaurantCardData } from "./entities/RestaurantCardData";
 
 function App() {
-    const restaurantCards = restaurantData.map(
+    const itemsPerPage = 20;
+    const [currPage, setCurrPage] = useState(1);
+    const startIndex = (currPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const totalPages = Math.ceil(restaurantData.length / itemsPerPage);
+
+    const handleNextPage = () => {
+        if (currPage < totalPages) {
+            setCurrPage(currPage + 1);
+        }
+    };
+
+    const handlePrevPage = () => {
+        if (currPage > 1) {
+            setCurrPage(currPage - 1);
+        }
+    };
+
+    const restaurantCards = restaurantData.slice(startIndex, endIndex).map(
         (restaurantCard: RestaurantCardData, index: number) => {
             return <RestaurantCard key={index} {...restaurantCard} />;
         }
@@ -14,6 +34,11 @@ function App() {
         <div>
             <Navbar />
             <section className="card-container">{restaurantCards}</section>
+            <div className="footer">
+                {currPage !== 1 && <button onClick={handlePrevPage} className="page-button">Previous</button >}
+                <span className="page-number">{currPage}/{totalPages}</span>
+                {currPage !== totalPages && <button onClick={handleNextPage} className="page-button">Next</button>}
+            </div>
         </div>
     );
 }
